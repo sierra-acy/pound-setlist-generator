5cc616bee573f9b0d94598764352e507f3c5c3de
Sierra Acy <sierra.acy@gmail.com>
2023-10-02 19:29:18 -0700

    converted to flask with dummy responses

--- setlist-generator/api/api.py
+++ setlist-generator/api/api.py
@@ -1,13 +1,54 @@
-import time
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+@app.route('/pound-setlist', methods=['GET','PUT'])
+def generate_pound_setlist():
+    """ Generate or Update POUND setlsit """
+    if(request.method == 'GET'):
+        DUMMY_SETLIST_DATA_POUND = [
+            {"id":1, "type":"warmup", "name":"songname1", "artist":"artist1"},
+            {"id":2, "type":"set", "level":"1", "name":"songname2", "artist":"artist2"}
+        ]
+        return DUMMY_SETLIST_DATA_POUND
+    elif(request.method == 'PUT'):
+        DUMMY_REPLACED_SETLIST_DATA_POUND = [
+            {"id":5, "name":"DummySong1", "artist":"DummyArtist1","type":"DummyType1", "level":"DummyLevel1"},
+            {"id":2, "type":"set", "level":"1", "name":"songname2", "artist":"artist2"}
+        ]
+        return DUMMY_REPLACED_SETLIST_DATA_POUND
 
-@app.route("/")
-def index():
-    """ index """
-    return "Congratulations, it's a web app!"
+@app.route('/pom-setlist', methods=['GET','PUT'])
+def generate_pom_setlist():
+    """ Generate PomSquad Setlist """
+    if(request.method == 'GET'):
+        DUMMY_SETLIST_DATA_POM = [
+        {"id":3, "type":"prancing", "name":"songname1", "artist":"artist1"},
+        {"id":4, "name":"songname2", "artist":"artist2"}
+        ]
+        return DUMMY_SETLIST_DATA_POM
+    elif(request.method == 'PUT'):
+        DUMMY_REPLACED_SETLIST_DATA_POUND = [
+            {"id":5, "name":"DummySong1", "artist":"DummyArtist1","type":"DummyType1", "level":"DummyLevel1"},
+            {"id":2, "type":"set", "level":"1", "name":"songname2", "artist":"artist2"}
+        ]
+        return DUMMY_REPLACED_SETLIST_DATA_POUND
+    
+
+@app.route('/pound-replacement-options')
+def find_pound_replacement_options():
+    """ Find list of matching replacements """
+    DUMMYREPLACEMENTOPTIONS= [
+        {"id":5, "name":"DummySong1", "artist":"DummyArtist1","type":"DummyType1", "level":"DummyLevel1"},
+        {"id":6, "name":"DummySong2", "artist":"DummyArtist2","type":"DummyType1", "level":"DummyLevel1"}
+    ]
+    return DUMMYREPLACEMENTOPTIONS
+
+@app.route('/pom-replacement-options')
+def find_pom_replacement_options():
+    """ Find list of matching replacements """
+    DUMMYREPLACEMENTOPTIONS= [
+        {"id":5, "name":"DummySong1", "artist":"DummyArtist1","type":"DummyType1", "level":"DummyLevel1"},
+        {"id":6, "name":"DummySong2", "artist":"DummyArtist2","type":"DummyType1", "level":"DummyLevel1"}
+    ]
+    return DUMMYREPLACEMENTOPTIONS
--- setlist-generator/src/App.js
+++ setlist-generator/src/App.js
@@ -38,32 +38,25 @@ function Track({ name, artist, type, level }) {
 }
 
 function Settings( { classType, settingsData, setSetlistData }) {
-  // const TEMP_SETTINGS_DATA_POUND = [
-  //   {label:"Class Difficulty", type:"radio", options:["Beginner", "Intermediate", "Advanced"]},
-  //   {label: "Class Length", type:"radio", options:["15", "30", "45"]},
-  //   {label: "Setlist Version", type:"radio", options:["A", "B"]},
-  //   {label: "Include Arm Track", type:"checkbox", name:"includeArmTrack"}
-  // ]
-
-  // const TEMP_SETTINGS_DATA_POM = [
-  //   {label:"Class Length", type:"radio", options:["15", "30", "50"]}
-  // ]
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    let generatedData;
+    let endpoint = '';
     if(classType === "pound") {
       // generate pound setlist
-      generatedData = DUMMY_SETLIST_DATA_POUND;
+      endpoint = 'pound-setlist';
     } else if(classType === "pom") {
       // generate pom setlist
-      generatedData = DUMMY_SETLIST_DATA_POM;
-    } else {
-      generatedData = "";
+      endpoint = 'pom-setlist';
+  } else {
+      console.log("classType not found");
     }
-    console.log("setting setlistdata in Settings");
-    setSetlistData(generatedData);
+
+    fetch('/' + endpoint).then(res => res.json()).then(data => {
+      console.log("setting setlistdata in Settings");
+      setSetlistData(data);
+    });
   }
 
   let settings = settingsData.map(setting => {
@@ -86,13 +79,24 @@ function Settings( { classType, settingsData, setSetlistData }) {
   );
 }
 
-function Setlist({ setlistData, setReplacementOptions, setIsReplace, setTrackToReplace }) {
+function Setlist({ setlistData, setReplacementOptions, setIsReplace, setTrackToReplace, classType }) {
   function handleReplace(trackData) {
     setTrackToReplace(trackData);
     // generate list of replacements using track data
-    console.log("setting replacementoptions and isreplace in Setlist");
-    setReplacementOptions(DUMMYREPLACEMENTOPTIONS);
-    setIsReplace(true);
+    let endpoint = '';
+    if(classType === "pound") {
+      endpoint = '/pound-replacement-options'
+    } else if(classType === "pom") {
+      endpoint = '/pom-replacement-options'
+    } else {
+      console.log("class type not found");
+    }
+
+    fetch(endpoint).then(res => res.json()).then(data => {
+      console.log("setting replacementoptions and isreplace in Setlist");
+      setReplacementOptions(data);
+      setIsReplace(true);
+    })
   }
 
     return (
@@ -109,19 +113,34 @@ function Setlist({ setlistData, setReplacementOptions, setIsReplace, setTrackToR
     );
   }
 
-function ReplaceSection ({ trackToReplace, replacementOptions, setlistData, setSetlistData, setIsReplace }) {
+function ReplaceSection ({ trackToReplace, replacementOptions, setlistData, setSetlistData, setIsReplace, classType }) {
 
   function handleSubmit(e) {
     e.preventDefault();
     // newTrackId = e.target.id
     // find index of trackToReplace in setlist data
     // map( track => { if track.id === trackToReplace.id{ return getTrack(newTrackId)}} )???
-    const DUMMY_REPLACED_SETLIST_DATA_POUND = [
-      {"id":5, "name":"DummySong1", "artist":"DummyArtist1","type":"DummyType1", "level":"DummyLevel1"},
-      {"id":2, "type":"set", "level":"1", "name":"songname2", "artist":"artist2"}
-    ]
-    setSetlistData(DUMMY_REPLACED_SETLIST_DATA_POUND);
-    setIsReplace(false);
+    // POST to update backend -- retuns updated??
+  
+    const requestOptions = {
+      method: 'PUT',
+      body: trackToReplace
+    };
+
+    let endpoint = '';
+    if(classType === 'pound') {
+      endpoint = '/pound-setlist';
+    } else if(classType === 'pom') {
+      endpoint = '/pom-setlist';
+    } else {
+      console.log('classtype not found');
+    }
+
+    fetch(endpoint, requestOptions).then(res => res.json()).then(data => {
+      setSetlistData(data);
+      setIsReplace(false);
+    });
+    
   }
 
   let prompt = "Which ";
@@ -164,7 +183,7 @@ function SetlistGeneratorSection({ classType, setlistData, setSetlistData, setRe
   return(
     <div> 
       <div className="generator">
-        <Setlist setlistData={setlistData} setReplacementOptions={setReplacementOptions} setIsReplace={setIsReplace} setTrackToReplace={setTrackToReplace}/>
+        <Setlist setlistData={setlistData} setReplacementOptions={setReplacementOptions} setIsReplace={setIsReplace} setTrackToReplace={setTrackToReplace} classType={classType}/>
         <Settings classType={classType} settingsData={settingsData} setSetlistData={setSetlistData}/>
       </div>
     </div>
@@ -192,7 +211,7 @@ function MainContentSection({ classType }){
   
     let section = <></>
     if(isReplace) {
-      section = <ReplaceSection setlistData={setlistData} replacementOptions={replacementOptions} onSetlistChange={setSetlistData} setIsReplace={setIsReplace} trackToReplace={trackToReplace} setSetlistData={setSetlistData}/>;
+      section = <ReplaceSection setlistData={setlistData} replacementOptions={replacementOptions} onSetlistChange={setSetlistData} setIsReplace={setIsReplace} trackToReplace={trackToReplace} setSetlistData={setSetlistData} classType={classType}/>;
     } else if(classType.length !== 0) {
       section = <SetlistGeneratorSection classType={classType} setlistData={setlistData} setSetlistData={setSetlistData} setReplacementOptions={setReplacementOptions} setIsReplace={setIsReplace} setTrackToReplace={setTrackToReplace}/>  
     }
@@ -236,16 +255,6 @@ function App() {
 const DUMMYREPLACEMENTOPTIONS= [
   {"id":5, "name":"DummySong1", "artist":"DummyArtist1","type":"DummyType1", "level":"DummyLevel1"},
   {"id":6, "name":"DummySong2", "artist":"DummyArtist2","type":"DummyType1", "level":"DummyLevel1"}
-]
-
-const DUMMY_SETLIST_DATA_POUND = [
-  {"id":1, "type":"warmup", "name":"songname1", "artist":"artist1"},
-  {"id":2, "type":"set", "level":"1", "name":"songname2", "artist":"artist2"}
-]
-
-const DUMMY_SETLIST_DATA_POM = [
-  {"id":3, "type":"prancing", "name":"songname1", "artist":"artist1"},
-  {"id":4, "name":"songname2", "artist":"artist2"}
 ]
 
 const TEMP_SETTINGS_DATA_POUND = [
